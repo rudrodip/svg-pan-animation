dd339179fbb7ae6a9347420404fde370df1080fb
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-12 16:31:37 +0600

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,61 @@ import TimelineSVG from "./_components/timeline";
 
 export default function Home() {
   return (
-    <main className="relative w-full h-[100dvh] flex flex-col">
-      <h1 className="relative text-3xl md:text-5xl text-center tracking-tighter mt-16 md:mt-24">
-        Interactive SVG Pan Animation
-      </h1>
-      <span className="text-sm text-center mt-2">inspired from <a href="https://linear.app/plan" className="underline">linear</a></span>
-      <div className="flex justify-center items-center gap-5 mt-3">
-        <a href="https://github.com/rudrodip/svg-pan-animation" target="_blank" className="px-4 py-1 rounded-lg border border-zinc-500 bg-background">Github</a>
-        <a href="https://x.com/rds_agi" target="_blank" className="px-4 py-1 rounded-lg border border-zinc-500 bg-background">X / Twitter</a>
-      </div>
-      <div
-        id="container"
-        className="flex justify-center items-center overflow-hidden"
-        style={{
-          transformStyle: "preserve-3d",
-          transform: `rotateX(65deg)`
-        }}
-      >
-        <TimelineSVG />
+    <main className="relative w-full h-[100dvh] flex flex-col gap-16 md:gap-32">
+      <Hero />
+      <div className="w-full h-full flex justify-center items-center">
+        <TimelineScroll />
       </div>
     </main>
   );
 }
+
+const TimelineScroll = () => {
+  return (
+    <div className="flex justify-center items-center w-full max-h-[400px] overflow-hidden">
+      <div
+        className="flex justify-center items-center w-full"
+        style={{
+          maskImage:
+            "linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0))",
+          transformStyle: "preserve-3d",
+          transform: "rotateX(65deg)",
+        }}
+      >
+        <TimelineSVG />
+      </div>
+    </div>
+  );
+};
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col mt-[15dvh]">
+      <h1 className="relative text-3xl md:text-5xl text-center tracking-tight font-semibold">
+        Interactive SVG Pan Animation
+      </h1>
+      <span className="text-sm text-center mt-2">
+        inspired from{" "}
+        <a href="https://linear.app/plan" target="_blank" className="underline">
+          linear
+        </a>
+      </span>
+      <div className="flex justify-center items-center gap-5 mt-3 text-sm">
+        <a
+          href="https://github.com/rudrodip/svg-pan-animation"
+          target="_blank"
+          className="px-3 py-1 rounded border border-zinc-500 bg-background"
+        >
+          Github
+        </a>
+        <a
+          href="https://x.com/rds_agi"
+          target="_blank"
+          className="px-3 py-1 rounded border border-zinc-500 bg-background"
+        >
+          X / Twitter
+        </a>
+      </div>
+    </div>
+  );
+};
