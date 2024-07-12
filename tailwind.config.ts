175361931afedb72067b6fa6d89ceba02ece8b6e
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-12 13:47:31 +0600

    added colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        foreground: "rgb(var(--foreground))",
+        background: "rgb(var(--background))",
+        muted: "rgb(var(--muted))",
+        "muted-foreground": "rgb(var(--muted-foreground))",
+      },
     },
   },
   plugins: [],
